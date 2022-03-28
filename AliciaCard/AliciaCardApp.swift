478d0697db16c710e62d63717f7817eaa0a4c5bc
Alicia W <aw6@realvnc.com>
2022-03-28 10:51:10 +0100

    inital commit

---
+++ AliciaCard/AliciaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliciaCardApp.swift
+//  AliciaCard
+//
+//  Created by Alicia Windsor on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AliciaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
